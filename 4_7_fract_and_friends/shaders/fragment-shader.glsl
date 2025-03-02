35f1842f47e8de6cd244016b878bc172ccb2cbb5
costin_branzas <costin.branzas@qubiz.com>
2025-03-02 14:15:31 +0200

    finalized fract and other functions visualized on a graph

--- 4_7_fract_and_friends/shaders/fragment-shader.glsl
+++ 4_7_fract_and_friends/shaders/fragment-shader.glsl
@@ -7,6 +7,7 @@ void main() {
   vec3 white = vec3(1.0, 1.0, 1.0);
   vec3 red = vec3(1.0, 0.0, 0.0);
   vec3 blue = vec3(0.0, 0.0, 1.0);
+  vec3 yellow = vec3(1.0, 1.0, 0.0);
 
   vec3 finalColor = vec3(0.75);
   
@@ -18,14 +19,37 @@ void main() {
   // finalColor = vec3(verticalLines);
   // finalColor = mix(finalColor, vec3(horizontalLines), vec3(horizontalLines));
 
-  //short version
-  vec2 cell = fract(v_uv * 10.0); //this is 0 in the bottom left corner and goes to 1 towards top right
+  //grid
+  vec2 center = v_uv - 0.5;
+  vec2 cell = fract(center * resolution / 100.0); //this is 0 in the bottom left corner and goes to 1 towards top right
   cell = abs(cell - 0.5); // this moves the 0 point in the "middle" instead of the bottom left
   float distToCell = 1.0 - 2.0 * max(cell.x, cell.y); // this calculates the distance to the cell center, basically making center of cell black, and the margins white
   
   float cellLine = smoothstep(0.0, 0.05, distToCell);
 
+  float xAxis = smoothstep(0.0, 0.002, abs(v_uv.y - 0.5));
+  float yAxis = smoothstep(0.0, 0.002, abs(v_uv.x - 0.5));
+
+  //lines
+  vec2 pos = center * resolution / 100.0;
+  float value1 = pos.x;
+  // float value2 = abs(pos.x);
+  // float value2 = floor(pos.x);
+  // float value2 = ceil(pos.x);
+  // float value2 = round(pos.x);
+  // float value2 = fract(pos.x);
+  float value2 = mod(pos.x, 1.5);
+
+  float functionLine1 = smoothstep(0.0, 0.075, abs(pos.y - value1)); // linear function
+  float functionLine2 = smoothstep(0.0, 0.075, abs(pos.y - value2)); //the function defined above
+
   finalColor = mix(black, finalColor, cellLine);
+
+  finalColor = mix(blue, finalColor, xAxis);
+  finalColor = mix(blue, finalColor, yAxis);
+
+  finalColor = mix(yellow, finalColor, functionLine1);
+  finalColor = mix(red, finalColor, functionLine2);
 
   gl_FragColor = vec4(finalColor, 1.0);
 }
