750155e3bcfb6ae834aba9320b8e8996f32b3fe0
costin_branzas <costin.branzas@qubiz.com>
2025-03-22 17:05:55 +0200

    small adjustments

--- 7_0_simple_transformations_base/shaders/fragment-shader.glsl
+++ 7_0_simple_transformations_base/shaders/fragment-shader.glsl
@@ -1,6 +1,6 @@
 varying vec2 v_uv;
-
 varying vec3 v_normal;
+varying vec3 v_position;
 
 uniform vec2 resolution;
 
--- 7_0_simple_transformations_base/shaders/vertex-shader.glsl
+++ 7_0_simple_transformations_base/shaders/vertex-shader.glsl
@@ -1,13 +1,13 @@
 varying vec2 v_uv;
 varying vec3 v_normal;
+varying vec3 v_position;
 
 void main() {	
-  vec4 localPosition = vec4(position, 1.0);
+  vec3 localSpacePosition = position;
 
-  gl_Position = projectionMatrix * modelViewMatrix * localPosition;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(localSpacePosition, 1.0); // translates from local space to clip space
 
   v_uv = uv;
-  
   v_normal = (modelMatrix * vec4(normal, 0.0)).xyz; //translates the normal from local space to world space (i think)
-
+  v_position = (modelMatrix * vec4(position, 1.0)).xyz;
 }
