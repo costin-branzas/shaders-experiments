0b79a638d5008a187118cd094acf8140b51b38ea
costin_branzas <costin.branzas@qubiz.com>
2025-10-28 15:59:17 +0200

    basic noise as texture

---
+++ 9_3_0_noise_as_texture/shaders/fragment-shader.glsl
@@ -0,0 +1,62 @@
+
+varying vec2 v_uv;
+uniform vec2 resolution;
+uniform float time;
+
+vec3 BLACK = vec3(0.0, 0.0, 0.0);
+vec3 YELLOW = vec3(1.0, 1.0, 0.5);
+vec3 BLUE = vec3(0.25, 0.25, 1.0);
+vec3 RED = vec3(1.0, 0.25, 0.25);
+vec3 GREEN = vec3(0.25, 1.0, 0.25);
+vec3 PURPLE = vec3(1.0, 0.25, 1.0);
+
+float inverseLerp(float v, float minValue, float maxValue) {
+  return (v - minValue) / (maxValue - minValue);
+}
+
+float remap(float v, float inMin, float inMax, float outMin, float outMax) {
+  float t = inverseLerp(v, inMin, inMax);
+  return mix(outMin, outMax, t);
+}
+
+vec3 hash(vec3 p) {
+  p = vec3(dot(p, vec3(127.1, 311.7, 74.7)),
+           dot(p, vec3(269.5, 183.3, 246.1)),
+           dot(p, vec3(113.5, 271.9, 124.6)));
+
+  return -1.0 + 2.0 * fract(sin(p) * 43758.5453123);
+}
+
+float noise(in vec3 p) {
+  vec3 i = floor(p);
+  vec3 f = fract(p);
+
+  vec3 u = f * f * (3.0 - 2.0 * f);
+
+  return mix(mix(mix(dot(hash(i + vec3(0.0, 0.0, 0.0)), f - vec3(0.0, 0.0, 0.0)),
+                     dot(hash(i + vec3(1.0, 0.0, 0.0)), f - vec3(1.0, 0.0, 0.0)), u.x),
+                 mix(dot(hash(i + vec3(0.0, 1.0, 0.0)), f - vec3(0.0, 1.0, 0.0)),
+                     dot(hash(i + vec3(1.0, 1.0, 0.0)), f - vec3(1.0, 1.0, 0.0)), u.x), u.y),
+             mix(mix(dot(hash(i + vec3(0.0, 0.0, 1.0)), f - vec3(0.0, 0.0, 1.0)),
+                     dot(hash(i + vec3(1.0, 0.0, 1.0)), f - vec3(1.0, 0.0, 1.0)), u.x),
+                 mix(dot(hash(i + vec3(0.0, 1.0, 1.0)), f - vec3(0.0, 1.0, 1.0)),
+                     dot(hash(i + vec3(1.0, 1.0, 1.0)), f - vec3(1.0, 1.0, 1.0)), u.x), u.y), u.z);
+}
+
+
+void main() {
+  vec3 colour = vec3(0.0, 0.0, 0.0);
+
+  vec3 v_uv_with_time = vec3(v_uv.x * 10.0, v_uv.y * 10.0, time * 0.1);
+  float noiseSample = 0.0;
+
+  noiseSample = noise(v_uv_with_time);
+  noiseSample = remap(noiseSample, -1.0, 1.0, 0.0, 1.0);
+  
+  // colour = hash(vec3(floor(time*0.1)));// just a basic test to see how the hash function works together with time
+  
+  colour = vec3(noiseSample);
+
+
+  gl_FragColor = vec4(colour, 1.0);
+}
