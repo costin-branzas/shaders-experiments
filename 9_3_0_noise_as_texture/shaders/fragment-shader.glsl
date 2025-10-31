161875c31374f90dd429ca00726c43520faa94de
costin_branzas <costin.branzas@qubiz.com>
2025-10-31 15:10:40 +0200

    basic fbm

--- 9_3_0_noise_as_texture/shaders/fragment-shader.glsl
+++ 9_3_0_noise_as_texture/shaders/fragment-shader.glsl
@@ -43,18 +43,34 @@ float noise(in vec3 p) {
                      dot(hash(i + vec3(1.0, 1.0, 1.0)), f - vec3(1.0, 1.0, 1.0)), u.x), u.y), u.z);
 }
 
+float fbm(vec3 p) {
+  float total = 0.0;
+  float amplitude = 0.5;
+  float frequency = 1.0;
+
+  for (int i = 0; i < 6; i++) {
+    total += noise(p * frequency) * amplitude;
+    frequency *= 2.0;
+    amplitude *= 0.5;
+  }
+
+  return total;
+}
 
 void main() {
   vec3 colour = vec3(0.0, 0.0, 0.0);
 
-  vec3 v_uv_with_time = vec3(v_uv.x * 10.0, v_uv.y * 10.0, time * 0.1);
+  vec3 v_uv_with_time = vec3(v_uv.x * 10.0, v_uv.y * 10.0, time * 0.5);
   float noiseSample = 0.0;
 
-  noiseSample = noise(v_uv_with_time);
+  // noiseSample = noise(v_uv_with_time);
+  noiseSample = fbm(v_uv_with_time);
   noiseSample = remap(noiseSample, -1.0, 1.0, 0.0, 1.0);
   
   // colour = hash(vec3(floor(time*0.1)));// just a basic test to see how the hash function works together with time
   
+  // noiseSample = smoothstep(0.5, 0.99, noiseSample); // experimenting with smoothstep to create a threshold effect
+
   colour = vec3(noiseSample);
 
 
