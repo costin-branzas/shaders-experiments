124ef0e8b1606196d813b2c19036013197f0e2a5
costin_branzas <costin.branzas@qubiz.com>
2025-11-10 11:15:11 +0200

    gradient noise - renamings from value noise but same functionality

--- 9_1_4_better_understanding_of_gradient_noise/shaders/fragment-shader.glsl
+++ 9_1_4_better_understanding_of_gradient_noise/shaders/fragment-shader.glsl
@@ -3,29 +3,7 @@ uniform vec2 resolution;
 uniform float time;
 
 
-float inverseLerp(float v, float minValue, float maxValue) {
-  return (v - minValue) / (maxValue - minValue);
-}
-
-float remap(float v, float inMin, float inMax, float outMin, float outMax) {
-  float t = inverseLerp(v, inMin, inMax);
-  return mix(outMin, outMax, t);
-}
-
-vec3 hash(vec3 p) {
-  p = vec3(dot(p, vec3(127.1, 311.7, 74.7)),
-           dot(p, vec3(269.5, 183.3, 246.1)),
-           dot(p, vec3(113.5, 271.9, 124.6)));
-
-  return -1.0 + 2.0 * fract(sin(p) * 43758.5453123);
-}
-
-float randomSimon(vec2 p) {
-  p = 50.0 * fract(p * 0.3183099 + vec2(0.71, 0.113));
-  return -1.0 + 2.0 * fract(p.x * p.y * (p.x + p.y));
-}
-
-vec3 randomYouTube(uvec3 v) {
+vec3 random(uvec3 v) {
   v = v * 1664525u + 1013904223u;
   v.x += v.y*v.z; v.y += v.z*v.x; v.z += v.x*v.y;
   v ^= v >> 16u;
@@ -33,24 +11,76 @@ vec3 randomYouTube(uvec3 v) {
   return vec3(v) * (1.0/float(0xffffffffu));
 }
 
-vec3 valueNoise(vec2 uv, float gridSize) {
+// vec3 valueNoise(vec2 uv, float gridSize) {
+//   // gridSize = 8.0; // grid size can be thought of as "frequency" because it basically tells us how many grid cells fit in 1 unit of UV space or how often the noise changes
+//   vec2 gridCoords = uv * gridSize;
+
+//   vec2 gridPosBase = floor(gridCoords);
+//   vec2 gridPosDetail = fract(gridCoords);
+
+  
+
+//   vec3 bottomLeftCornerColour = random(uvec3(gridPosBase.x, gridPosBase.y, 0.0));
+//   vec3 bottomRightCornerColour = random(uvec3(gridPosBase.x + 1.0, gridPosBase.y, 0.0));
+//   vec3 topLeftCornerColour = random(uvec3(gridPosBase.x, gridPosBase.y + 1.0, 0.0));
+//   vec3 topRightCornerColour = random(uvec3(gridPosBase.x + 1.0, gridPosBase.y + 1.0, 0.0));
+
+//   gridPosDetail = smoothstep(0.0, 1.0, gridPosDetail);
+
+//   vec3 bottomColour = mix(bottomLeftCornerColour, bottomRightCornerColour, gridPosDetail.x);
+//   vec3 topColour = mix(topLeftCornerColour, topRightCornerColour, gridPosDetail.x);
+  
+//   vec3 finalColour = mix(bottomColour, topColour, gridPosDetail.y);
+
+//   return finalColour;
+// }
+
+
+// vec3 fractalValueNoise(vec2 uv, float gridSize, float octaves) {
+//   vec3 composedValueNoise = vec3(0.0);
+
+//   float amplitude = 0.5;
+//   float frequency = 1.0;
+
+//   for (float i = 0.0; i < octaves; i += 1.0) {
+//     composedValueNoise += amplitude * valueNoise(uv, gridSize * frequency); // grid size is basically frequency, we mutiply it to increase it, so frequency would be better called "frequency coefficient"
+//     amplitude *= 0.5;
+//     frequency *= 2.0;
+//   }
+  
+//   return composedValueNoise;
+// }
+
+
+vec3 gradientNoise(vec2 uv, float gridSize) {
   // gridSize = 8.0; // grid size can be thought of as "frequency" because it basically tells us how many grid cells fit in 1 unit of UV space or how often the noise changes
   vec2 gridCoords = uv * gridSize;
 
   vec2 gridPosBase = floor(gridCoords);
   vec2 gridPosDetail = fract(gridCoords);
 
+  // vec3 bottomLeftCornerColour = random(uvec3(gridPosBase.x, gridPosBase.y, 0.0));
+  // vec3 bottomRightCornerColour = random(uvec3(gridPosBase.x + 1.0, gridPosBase.y, 0.0));
+  // vec3 topLeftCornerColour = random(uvec3(gridPosBase.x, gridPosBase.y + 1.0, 0.0));
+  // vec3 topRightCornerColour = random(uvec3(gridPosBase.x + 1.0, gridPosBase.y + 1.0, 0.0));
+
+  vec3 bottomLeftCornerVector = random(uvec3(gridPosBase.x, gridPosBase.y, 0.0));
+  vec3 bottomRightCornerVector = random(uvec3(gridPosBase.x + 1.0, gridPosBase.y, 0.0));
+  vec3 topLeftCornerVector = random(uvec3(gridPosBase.x, gridPosBase.y + 1.0, 0.0));
+  vec3 topRightCornerVector = random(uvec3(gridPosBase.x + 1.0, gridPosBase.y + 1.0, 0.0));
+
+  vec3 bottomLeftToCurrentPos = vec3(gridPosDetail, 0.0) - bottomLeftCornerVector;
+  vec3 bottomRightToCurrentPos = vec3(gridPosDetail - vec2(1.0, 0.0), 0.0) - bottomRightCornerVector;
+  vec3 topLeftToCurrentPos = vec3(gridPosDetail - vec2(0.0, 1.0), 0.0) - topLeftCornerVector;
+  vec3 topRightToCurrentPos = vec3(gridPosDetail - vec2(1.0, 1.0), 0.0) - topRightCornerVector;
+
   
 
-  vec3 bottomLeftCornerColour = randomYouTube(uvec3(gridPosBase.x, gridPosBase.y, 0.0));
-  vec3 bottomRightCornerColour = randomYouTube(uvec3(gridPosBase.x + 1.0, gridPosBase.y, 0.0));
-  vec3 topLeftCornerColour = randomYouTube(uvec3(gridPosBase.x, gridPosBase.y + 1.0, 0.0));
-  vec3 topRightCornerColour = randomYouTube(uvec3(gridPosBase.x + 1.0, gridPosBase.y + 1.0, 0.0));
 
-  gridPosDetail = smoothstep(0.0, 1.0, gridPosDetail);
+  // gridPosDetail = smoothstep(0.0, 1.0, gridPosDetail);
 
-  vec3 bottomColour = mix(bottomLeftCornerColour, bottomRightCornerColour, gridPosDetail.x);
-  vec3 topColour = mix(topLeftCornerColour, topRightCornerColour, gridPosDetail.x);
+  vec3 bottomColour = mix(bottomLeftCornerVector, bottomRightCornerVector, gridPosDetail.x);
+  vec3 topColour = mix(topLeftCornerVector, topRightCornerVector, gridPosDetail.x);
   
   vec3 finalColour = mix(bottomColour, topColour, gridPosDetail.y);
 
@@ -58,35 +88,32 @@ vec3 valueNoise(vec2 uv, float gridSize) {
 }
 
 
-vec3 fractalValueNoise(vec2 uv, float gridSize, float octaves) {
-  vec3 composedValueNoise = vec3(0.0);
+vec3 fractalGradientNoise(vec2 uv, float gridSize, float octaves) {
+  vec3 composedGradientNoise = vec3(0.0);
 
   float amplitude = 0.5;
   float frequency = 1.0;
 
   for (float i = 0.0; i < octaves; i += 1.0) {
-    composedValueNoise += amplitude * valueNoise(uv, gridSize * frequency); // grid size is basically frequency, we mutiply it to increase it, so frequency would be better called "frequency coefficient"
+    composedGradientNoise += amplitude * gradientNoise(uv, gridSize * frequency); // grid size is basically frequency, we mutiply it to increase it, so frequency would be better called "frequency coefficient"
     amplitude *= 0.5;
     frequency *= 2.0;
   }
   
-
-  return composedValueNoise;
+  return composedGradientNoise;
 }
-
 
 
 void main() {
   vec3 colour = vec3(0.0, 0.0, 0.0);
   
-  // colour = randomYouTube(a); // noise at pixel level
-
-
   // colour = valueNoise(v_uv, 8.0);
   // colour = valueNoise(v_uv + time * 0.1, 8.0); // animated noise - the pos shifts with time
 
-  colour = fractalValueNoise(v_uv, 4.0, 10.0);
+  // colour = fractalValueNoise(v_uv, 1.0, 8.0);
+  // colour = fractalValueNoise(v_uv, 4.0, 1.0);
 
+  colour = gradientNoise(v_uv, 3.0); // animated noise - the pos shifts with time
 
   gl_FragColor = vec4(colour, 1.0);
 }
